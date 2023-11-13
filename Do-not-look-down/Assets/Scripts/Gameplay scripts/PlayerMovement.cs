bb0245b6a9b1d194580fb5d1674e60ee4ebcb5b1
AMozg <andmozgawa@gmail.com>
2023-11-13 19:55:39 +0100

    Adding more jumping mechanics and working on a one-sided platforms

--- Do-not-look-down/Assets/Scripts/Gameplay scripts/PlayerMovement.cs
+++ Do-not-look-down/Assets/Scripts/Gameplay scripts/PlayerMovement.cs
@@ -4,27 +4,29 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Serialization;
 
-public class PlayerBehaviour : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
     private float horizontal;
+    
+    [Header("Movespeed")]
     private float speed = 8f;
-    private float jumpingPower = 16f;
+    
+    [Header("Jumping Power")]    
+    private float jumpingPower = 12f;
 
-    [SerializeField] private Transform groundCheck;
-    [SerializeField] private Transform wallCheck;
+    [Header("Object References")]
     [SerializeField] private Rigidbody2D rigidBody;
+    
+    [Header("Grounded")]
+    [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
-    [SerializeField] private LayerMask wallLayer;
-
-
+    
+    [SerializeField]public float raycasterDistance;
+    
+    
     private bool IsGrounded()
     {
         return Physics2D.OverlapCircle(groundCheck.position, 1f, groundLayer);
-    }
-
-    private bool IsNearWall()
-    {
-        return Physics2D.OverlapCircle(wallCheck.position, 1f, wallLayer);
     }
 
     private void Update()
@@ -40,22 +42,26 @@ public class PlayerBehaviour : MonoBehaviour
         {
             rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y*0.5f);
         }
+        
+        
+        
+        RaycastHit2D hitUp = Physics2D.Raycast(transform.position, transform.up, raycasterDistance, groundLayer);
+        RaycastHit2D hitDown = Physics2D.Raycast(transform.position, -transform.up, raycasterDistance, groundLayer);
+
+
+        if (hitUp.collider != null)
+        {
+            GetComponent<BoxCollider2D>().isTrigger = true;
+        }
+        if (hitDown.collider == null&&hitUp.collider== null)
+        {
+            GetComponent<BoxCollider2D>().isTrigger = false;
+        }
+
     }
 
     private void FixedUpdate()
     {
-        
-        if (rigidBody.velocity.y < 0f && IsNearWall())
-        {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 0.5f);
-
-        }
-
-        if (!IsNearWall())
-        {
-            rigidBody.velocity = new Vector2(rigidBody.velocity.x, rigidBody.velocity.y * 2f);
-        }
-        
         rigidBody.velocity = new Vector2(horizontal*speed, rigidBody.velocity.y);
     }
 }
